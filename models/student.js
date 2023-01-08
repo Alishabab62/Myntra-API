4f3f46ebbefac80b52f6a654c0aaaa163b87f508
alishabab62 <alishabab62@gmail.com>
2023-01-08 18:03:21 +0530

    password hashing

--- models/student.js
+++ models/student.js
@@ -8,6 +8,10 @@ const studentSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  password:{
+    type:String,
+    required:true
+  }
 });
 mongoose.set('strictQuery', false);
 module.exports = mongoose.model("Student", studentSchema);
--- routers/routes.js
+++ routers/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Student = require("../models/student");
 const mongoose = require("mongoose");
+const bcrypt=require('bcrypt');
 
 router.get("/faculty", (req, res) => {
   Student.find().then((result) => {
@@ -31,28 +32,99 @@ router.delete("/faculty/:id", (req, res) => {
   });
 });
 
-router.post("/student", async (req, res) => {
-  const student = new Student({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    phone: req.body.phone,
-  });
-  const existingUser = await Student.findOne({ phone: req.body.phone });
-  if (!existingUser) {
-    student
-      .save()
-      .then((result) => {
-        console.log(result);
-        res.status(200).json({
-          success: true,
+
+
+router.put('/:id',(req,res)=>{
+  Student.findOneAndUpdate({phone:req.params.id},{
+    $set:{
+      name: req.body.name,
+      phone: req.body.phone,
+      password:req.body.password
+    }
+  }).then((result)=>{
+    res.status(200).json({
+      result:result
+    })
+  }).catch((err)=>{
+    console.log(err)
+    res.status(500).json({
+      error:err
+    })
+  })
+})
+
+
+// for storing password in normal form  
+
+// router.post("/student", async (req, res) => {
+  
+//   const student = new Student({
+//     _id: new mongoose.Types.ObjectId(),
+//     name: req.body.name,
+//     phone: req.body.phone,
+//     password:req.body.password
+//   });
+//   const existingUser = await Student.findOne({ phone: req.body.phone });
+//   if (!existingUser) {
+//     student
+//       .save()
+//       .then((result) => {
+//         console.log(result);
+//         res.status(200).json({
+//           success: true,
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   } else {
+//     console.log(req.body);
+//   }
+// });
+
+
+
+
+// for storing passowrd in hash
+
+router.post("/student",  (req, res) => {
+  bcrypt.hash(req.body.password,10, async (err,hash)=>{
+   if(err){
+    return res.status(500).json({
+      error:err
+    })
+   }
+   else{
+    const student = new Student({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      phone: req.body.phone,
+      password:hash
+    });
+    const existingUser = await Student.findOne({ phone: req.body.phone });
+    if (!existingUser) {
+      student
+        .save()
+        .then((result) => {
+          console.log(result);
+          res.status(200).json({
+            success: true,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } else {
-    console.log(req.body);
-  }
+    } else {
+      console.log(req.body);
+    }
+   }
+  })
+  
 });
+
+
+
+
+
 
 module.exports = router;
